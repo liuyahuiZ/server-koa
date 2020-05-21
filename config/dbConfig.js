39840586ba7919e94e6bce44ff96a4e91248dcb0
刘亚辉 <liuyh@jfpal.com>
2020-05-21 10:25:23 +0800

    修改数据库配置

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,10 @@ let dbName = "nodeapi";
 let dbHost = "mongodb://localhost/";
 let mongoose = require("mongoose");
 exports.connect = function(request, response) {
-    mongoose.connect(dbHost + dbName, { useMongoClient: true }); // useMongoClient防止报错
+    mongoose.connect(dbHost + dbName, { useMongoClient: true, server: {
+      auto_reconnect: true,
+      poolSize: 30
+    } }); // useMongoClient防止报错
     let db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function (callback) {
